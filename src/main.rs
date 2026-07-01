c1024c7ed594571748d490da7f14dfe2fa3d4127
dsw7 <dds3443@hotmail.com>
2026-07-01 04:27:35 -0700

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(about = "Program for editing individual files using LLMs.", version)]
+struct Cli {
+    #[arg(value_name = "FILE-TO-EDIT")]
+    file_to_edit: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
