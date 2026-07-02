001dfbbd479ac1fa96256c87c27b4f3bad24282c
dsw7 <dds3443@hotmail.com>
2026-07-02 01:51:16 -0700

    Separate request body concerns

--- src/query_openai.rs
+++ src/query_openai.rs
@@ -3,7 +3,9 @@ use std::error::Error;
 use std::time::Duration;
 
 use reqwest::blocking::Client;
-use serde_json::json;
+use serde_json::{self, json};
+
+use crate::params::Parameters;
 
 fn get_api_key() -> Result<String, String> {
     const OPENAI_API_KEY: &str = "OPENAI_API_KEY";
@@ -15,23 +17,29 @@ fn get_api_key() -> Result<String, String> {
     }
 }
 
-pub fn query_openai() -> Result<(), Box<dyn Error>> {
-    let api_key = get_api_key()?;
-
+fn get_request_body(params: &Parameters) -> serde_json::Value {
     let input = json!([
         { "role": "system", "content": "You are a helpful assistant." },
-        { "role": "user", "content": "What is 3 + 5?" }
+        { "role": "user", "content": params.prompt }
     ]);
 
+    json!({ "model": params.model, "input": input })
+}
+
+pub fn query_openai(params: &Parameters) -> Result<(), Box<dyn Error>> {
+    let api_key = get_api_key()?;
     let client = Client::builder().timeout(Duration::from_secs(10)).build()?;
+
+    let request_body = get_request_body(&params);
     let response = client
         .post("https://api.openai.com/v1/responses")
         .header("Content-Type", "application/json")
         .header("Authorization", format!("Bearer {}", api_key))
-        .json(&json!({ "model": "gpt-4o", "input": input }))
+        .json(&request_body)
         .send()?;
 
     let response_text = response.text()?;
     println!("{}", response_text);
+
     Ok(())
 }
