9eee4de89e92b862a3e6d95da4532747d4f6c9df
dsw7 <dds3443@hotmail.com>
2026-07-02 00:33:02 -0700

    Add basic API call

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::error::Error;
 use std::path::PathBuf;
+use std::time::Duration;
 
 use clap::Parser;
+use reqwest::blocking::Client;
 
 #[derive(Parser, Debug)]
 #[command(about = "Program for editing individual files using LLMs.", version)]
@@ -9,6 +12,16 @@ struct Cli {
     file_to_edit: PathBuf,
 }
 
+fn query_llm() -> Result<(), Box<dyn Error>> {
+    let client = Client::builder().timeout(Duration::from_secs(10)).build()?;
+
+    let response = client.get("https://www.rust-lang.org").send()?;
+    println!("Body text: {}", response.text()?);
+    Ok(())
+}
+
 fn main() {
     let cli = Cli::parse();
+
+    let _ = query_llm();
 }
