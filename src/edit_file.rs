c56f6215b816365b1b3d00694a712768f2fef7a0
dsw7 <dds3443@hotmail.com>
2026-07-04 03:06:20 -0700

    Read the file contents

--- src/edit_file.rs
+++ src/edit_file.rs
@@ -1,8 +1,8 @@
 use crate::params::Parameters;
 use crate::query_openai::query_openai;
 
-use std::fs::File;
-use std::io::{self, Write};
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
 use std::path::PathBuf;
 
 fn create_file_with_prompt(file: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
@@ -23,6 +23,15 @@ fn create_file_with_prompt(file: &PathBuf) -> Result<(), Box<dyn std::error::Err
     Ok(())
 }
 
+fn read_file_to_string(file: &PathBuf) -> Result<String, io::Error> {
+    let mut file_handle = File::open(file)?;
+
+    let mut file_content = String::new();
+    file_handle.read_to_string(&mut file_content)?;
+
+    Ok(file_content)
+}
+
 pub fn edit_file(
     file_to_edit: &PathBuf,
     params: &Parameters,
@@ -30,6 +39,8 @@ pub fn edit_file(
     if !file_to_edit.exists() {
         create_file_with_prompt(&file_to_edit)?;
     }
+
+    let text_to_edit = read_file_to_string(&file_to_edit)?;
 
     let results = query_openai(&params)?;
 
@@ -40,6 +51,8 @@ pub fn edit_file(
         "Description of what was done: {}",
         results.description_of_what_was_done
     );
+
+    fs::write(file_to_edit, results.code)?;
 
     Ok(())
 }
