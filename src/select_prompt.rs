135825206b72dc41e4d939ff82fed713a6131312
dsw7 <dds3443@hotmail.com>
2026-07-06 23:19:48 -0700

    Separate concerns

--- src/select_prompt.rs
+++ src/select_prompt.rs
@@ -8,8 +8,8 @@ fn load_prompt_from_stdin() -> Result<String, io::Error> {
     let mut prompt_from_stdin = String::new();
     io::stdin().read_line(&mut prompt_from_stdin)?;
 
-    let prompt_trimmed = prompt_from_stdin.trim().to_string();
-    Ok(prompt_trimmed)
+    let prompt = prompt_from_stdin.trim().to_string();
+    Ok(prompt)
 }
 
 fn load_prompt_from_file() -> Result<String, io::Error> {
@@ -24,12 +24,30 @@ fn load_prompt_from_file() -> Result<String, io::Error> {
     ))
 }
 
+fn is_prompt_empty(user_prompt: Result<String, io::Error>) -> Result<String, io::Error> {
+    match user_prompt {
+        Ok(prompt) => {
+            if prompt.is_empty() {
+                Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Prompt cannot be empty",
+                ))
+            } else {
+                Ok(prompt)
+            }
+        }
+        Err(error) => Err(error),
+    }
+}
+
 pub fn select_prompt(user_prompt_from_cli: &Option<String>) -> Result<String, io::Error> {
-    match user_prompt_from_cli {
+    let user_prompt = match user_prompt_from_cli {
         Some(prompt) => Ok(prompt.to_string()),
         None => match load_prompt_from_file() {
             Ok(file_prompt) => Ok(file_prompt),
             Err(_) => load_prompt_from_stdin(),
         },
-    }
+    };
+
+    is_prompt_empty(user_prompt)
 }
