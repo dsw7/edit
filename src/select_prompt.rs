c73de8a6dd67208dd8b99d1c2a158ee18b1754a6
dsw7 <dds3443@hotmail.com>
2026-07-31 20:55:28 -0700

    Update prompt selection

--- src/select_prompt.rs
+++ src/select_prompt.rs
@@ -1,35 +1,42 @@
 use std::fs;
 use std::io::{self, Write};
+use std::path::Path;
 
 use anyhow::{self, Context};
-
-fn load_prompt_from_file() -> anyhow::Result<String> {
-    if fs::metadata("Inputfile").is_ok() {
-        return fs::read_to_string("Inputfile").context("Failed to read the content of Inputfile");
-    }
-
-    Err(anyhow::anyhow!("Inputfile does not exist"))
-}
 
 fn load_prompt_from_stdin() -> anyhow::Result<String> {
     print!(">>> ");
     io::stdout().flush().context("Failed to flush stdout")?;
 
-    let mut prompt_from_stdin = String::new();
+    let mut prompt = String::new();
     io::stdin()
-        .read_line(&mut prompt_from_stdin)
+        .read_line(&mut prompt)
         .context("Failed to read line from stdin")?;
 
-    Ok(prompt_from_stdin)
+    Ok(prompt)
+}
+
+fn load_prompt_from_file() -> anyhow::Result<String> {
+    println!("Found Inputfile in current directory. Reading instructions from this file");
+
+    let prompt =
+        fs::read_to_string("Inputfile").context("Failed to read the content of Inputfile")?;
+
+    Ok(prompt)
+}
+
+fn load_prompt_from_file_or_stdin() -> anyhow::Result<String> {
+    if Path::new("Inputfile").exists() {
+        load_prompt_from_file()
+    } else {
+        load_prompt_from_stdin()
+    }
 }
 
 pub fn select_prompt(user_prompt_from_cli: &Option<String>) -> anyhow::Result<String> {
     let user_prompt = match user_prompt_from_cli {
         Some(prompt) => prompt.to_string(),
-        None => match load_prompt_from_file() {
-            Ok(file_prompt) => file_prompt,
-            Err(_) => load_prompt_from_stdin()?,
-        },
+        None => load_prompt_from_file_or_stdin()?,
     };
 
     let user_prompt = user_prompt.trim().to_string();
