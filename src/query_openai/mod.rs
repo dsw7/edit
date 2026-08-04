8f9a324caf0a369d35c7133b460075b7e342f385
dsw7 <dds3443@hotmail.com>
2026-08-03 22:37:26 -0700

    Separate request

--- src/query_openai/mod.rs
+++ src/query_openai/mod.rs
@@ -1,5 +1,28 @@
+mod request;
 mod response;
-mod run_query;
 
+use std::time::Duration;
+
+use reqwest::blocking::Client;
+
+use crate::utils::load_api_key;
+
+use request::set_up_request_body;
 pub use response::OpenAIResults;
-pub use run_query::run_query;
+use response::deserialize_json_response;
+
+pub fn run_query(prompt: &String, model: &String) -> anyhow::Result<OpenAIResults> {
+    let api_key = load_api_key("OPENAI_API_KEY")?;
+    let client = Client::builder().timeout(Duration::from_secs(10)).build()?;
+
+    let request_body = set_up_request_body(prompt, model);
+    let response = client
+        .post("https://api.openai.com/v1/responses")
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {api_key}"))
+        .json(&request_body)
+        .send()?;
+
+    let raw_json = response.text()?;
+    deserialize_json_response(raw_json)
+}
--- src/query_openai/request.rs
+++ src/query_openai/request.rs
@@ -1,11 +1,4 @@
-use std::time::Duration;
-
-use reqwest::blocking::Client;
 use serde_json::json;
-
-use crate::utils::load_api_key;
-
-use super::response::{OpenAIResults, deserialize_json_response};
 
 fn get_structured_output_schema() -> serde_json::Value {
     json!({
@@ -38,7 +31,7 @@ Output:
 "
 }
 
-fn set_up_request_body(prompt: &String, model: &String) -> serde_json::Value {
+pub fn set_up_request_body(prompt: &String, model: &String) -> serde_json::Value {
     let structured_output_schema = get_structured_output_schema();
     let system_prompt = get_system_prompt();
 
@@ -48,20 +41,4 @@ fn set_up_request_body(prompt: &String, model: &String) -> serde_json::Value {
         "instructions": system_prompt,
         "text": structured_output_schema,
     })
-}
-
-pub fn run_query(prompt: &String, model: &String) -> anyhow::Result<OpenAIResults> {
-    let api_key = load_api_key("OPENAI_API_KEY")?;
-    let client = Client::builder().timeout(Duration::from_secs(10)).build()?;
-
-    let request_body = set_up_request_body(prompt, model);
-    let response = client
-        .post("https://api.openai.com/v1/responses")
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Bearer {api_key}"))
-        .json(&request_body)
-        .send()?;
-
-    let raw_json = response.text()?;
-    deserialize_json_response(raw_json)
 }
