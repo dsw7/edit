33fad8eb4de522e5807f1ea4f72d112524df7d5c
dsw7 <dds3443@hotmail.com>
2026-08-06 00:17:06 -0700

    Migrate new strategy

--- src/query_openai/response.rs
+++ src/query_openai/response.rs
@@ -3,6 +3,148 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 #[serde(untagged)]
+enum ApiResponse {
+    ErrorResponse(ErrorResponse),
+    SuccessResponse(Response),
+}
+
+#[derive(Deserialize, Debug)]
+struct ErrorResponse {
+    error: Error,
+}
+
+#[derive(Deserialize, Debug)]
+struct Error {
+    message: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    // ResponseStatus string may or may not exist
+    status: Option<String>,
+
+    // will always exist as ResponseError object or null
+    error: Option<ResponseError>,
+
+    // will always exist as object or null
+    incomplete_details: Option<IncompleteDetails>,
+
+    // will always exist
+    output: Vec<ResponseOutputMessage>,
+
+    // ResponseUsage object may or may not exist - populate with defaults
+    #[serde(default = "default_usage")]
+    usage: ResponseUsage,
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseError {
+    code: String,
+    message: String,
+}
+
+fn unpack_error(response: &Response) -> anyhow::Result<()> {
+    match &response.error {
+        Some(details) => anyhow::bail!(
+            "Query failed with code `{}` and message `{}`",
+            details.code,
+            details.message
+        ),
+        None => anyhow::bail!("Query failed. No details provided"),
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct IncompleteDetails {
+    reason: String,
+}
+
+fn unpack_incomplete_details(response: &Response) -> anyhow::Result<()> {
+    match &response.incomplete_details {
+        Some(details) => anyhow::bail!("Query incomplete: {}", details.reason),
+        None => anyhow::bail!("Query incomplete. No details provided"),
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseOutputMessage {
+    status: String,
+    content: Vec<TextOrRefusal>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+enum TextOrRefusal {
+    Text(ResponseOutputText),
+    Refusal(ResponseOutputRefusal),
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseOutputText {
+    text: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseOutputRefusal {
+    refusal: String,
+}
+
+fn unpack_output(response: &Response) -> anyhow::Result<()> {
+    for object in &response.output {
+        if object.status == "completed" {
+            return unpack_content(&object.content);
+        }
+    }
+
+    anyhow::bail!("Query completed but no completed message found!")
+}
+
+fn unpack_content(content: &[TextOrRefusal]) -> anyhow::Result<()> {
+    match &content[0] {
+        TextOrRefusal::Text(text) => println!("{}", text.text),
+        TextOrRefusal::Refusal(refusal) => anyhow::bail!("Query refused: {}", refusal.refusal),
+    }
+
+    Ok(())
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseUsage {
+    input_tokens: u32,
+    output_tokens: u32,
+}
+
+fn default_usage() -> ResponseUsage {
+    ResponseUsage {
+        input_tokens: 0,
+        output_tokens: 0,
+    }
+}
+
+fn unpack_response(response: &Response) -> anyhow::Result<()> {
+    match &response.status {
+        Some(status) => match status.as_str() {
+            "completed" => unpack_output(response),
+            "incomplete" => unpack_incomplete_details(response),
+            "failed" => unpack_error(response),
+            _ => anyhow::bail!(format!("Query did not finish. Status: {status}")),
+        },
+        None => anyhow::bail!("No status could be found in response"),
+    }
+}
+
+pub fn deserialize_json_response(raw_json: String) -> anyhow::Result<()> {
+    let response =
+        serde_json::from_str::<ApiResponse>(&raw_json).context("Failed to deserialize raw JSON")?;
+
+    match response {
+        ApiResponse::ErrorResponse(error) => anyhow::bail!(error.error.message),
+        ApiResponse::SuccessResponse(response) => unpack_response(&response),
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
 enum RawResponse {
     Success(SuccessResponse),
     Error(ErrorResponse),
