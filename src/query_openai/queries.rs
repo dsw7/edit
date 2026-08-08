701c68d1b54b5edfc89cb44ef21f75c2c05321f6
dsw7 <dds3443@hotmail.com>
2026-08-08 02:55:05 -0700

    Rename + fix tests

--- src/query_openai/queries.rs
+++ src/query_openai/queries.rs
@@ -105,37 +105,41 @@ pub fn edit_code_block(
 mod tests {
     use super::write_new_code;
 
-    fn assert_error_message(model: &str, prompt: &str, expected_error: &str) {
+    #[test]
+    fn test_write_new_code_invalid_model() {
+        let model = "foobar";
+        let prompt = "What is 3 + 5?";
+
         let result = write_new_code(model.to_string(), prompt.to_string());
         assert!(result.is_err());
 
         let error = result.unwrap_err();
-        assert_eq!(error.to_string(), expected_error);
-    }
-
-    #[test]
-    fn test_invalid_model() {
-        assert_error_message(
-            "foobar",
-            "What is 3 + 5?",
-            "The requested model 'foobar' does not exist.",
+        assert_eq!(
+            error.to_string(),
+            "The requested model 'foobar' does not exist."
         );
     }
 
     #[test]
-    fn test_incompatible_model() {
-        assert_error_message(
-            "gpt-3.5-turbo",
-            "What is 3 + 5?",
-            "Invalid parameter: 'text.format' of type 'json_schema' is not supported with model version `gpt-3.5-turbo`.",
+    fn test_write_new_code_incompatible_model() {
+        let model = "gpt-3.5-turbo";
+        let prompt = "What is 3 + 5?";
+
+        let result = write_new_code(model.to_string(), prompt.to_string());
+        assert!(result.is_err());
+
+        let error = result.unwrap_err();
+        assert_eq!(
+            error.to_string(),
+            "Invalid parameter: 'text.format' of type 'json_schema' is not supported with model version `gpt-3.5-turbo`."
         );
     }
 
     #[test]
-    fn test_valid_query() {
-        let model = String::from("gpt-4o");
-        let prompt = String::from("Print 'hello world' in Python.");
-        let result = write_new_code(model, prompt).unwrap();
+    fn test_write_new_code_valid_query() {
+        let model = "gpt-4o";
+        let prompt = "Print 'hello world' in Python.";
+        let result = write_new_code(model.to_string(), prompt.to_string()).unwrap();
         assert!(result.input_tokens > 0);
         assert!(result.output_tokens > 0);
         assert!(!result.description_of_what_was_done.is_empty());
