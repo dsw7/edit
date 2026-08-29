8ebd61d27aa5480a75ec6a15bd6daa4c1fd67f09
dsw7 <dds3443@hotmail.com>
2026-08-29 03:07:12 -0700

    Add struct for storing unpacked validation

--- src/query_ollama/structs.rs
+++ src/query_ollama/structs.rs
@@ -1,7 +1,6 @@
 #[derive(Debug)]
 pub struct ValidationResults {
-    pub input_tokens: u32,
-    pub output_tokens: u32,
+    pub total_duration: u32,
     pub reasoning: String,
     pub valid_instructions: bool,
 }
