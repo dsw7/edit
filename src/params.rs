5fae163743958ade8ce5f8dbd3d9cc903275a428
dsw7 <dds3443@hotmail.com>
2026-08-30 01:30:33 -0700

    Add validation fields to params

--- src/params.rs
+++ src/params.rs
@@ -5,4 +5,9 @@ pub struct Parameters {
     pub input_file: PathBuf,
     pub model: String,
     pub provider: String,
+
+    // Validation
+    pub ollama_host: String,
+    pub ollama_port: u16,
+    pub ollama_validation_model: String,
 }
