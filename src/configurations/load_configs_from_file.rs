d09620a4e032c69c12c5146eb5ed1dd3467d1c12
dsw7 <dds3443@hotmail.com>
2026-08-31 00:40:56 -0700

    Misc. rename

--- src/configurations/load_configs_from_file.rs
+++ src/configurations/load_configs_from_file.rs
@@ -19,7 +19,7 @@ where
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Configs {
+pub struct ConfigsFromFile {
     #[serde(deserialize_with = "check_not_empty")]
     pub provider: String,
 
@@ -47,14 +47,14 @@ pub struct OpenAI {
     pub code_edit_model: String,
 }
 
-pub fn load_configs() -> anyhow::Result<Configs> {
+pub fn load_configs() -> anyhow::Result<ConfigsFromFile> {
     let app_dir = program_files::get_app_dir()?;
     let config_file = program_files::get_config_file(&app_dir);
 
     let toml_str = fs::read_to_string(&config_file)
         .context(format!("cannot read {}", config_file.display()))?;
 
-    let configs = toml::from_str::<Configs>(&toml_str)
+    let configs = toml::from_str::<ConfigsFromFile>(&toml_str)
         .context(format!("failed to parse {}", config_file.display()))?;
 
     Ok(configs)
