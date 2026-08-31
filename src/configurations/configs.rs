c31a0e55b583f87c380c720b568faf838e5782b6
dsw7 <dds3443@hotmail.com>
2026-08-31 01:04:34 -0700

    Misc.

--- src/configurations/configs.rs
+++ src/configurations/configs.rs
@@ -1,14 +1,20 @@
 use std::path::PathBuf;
 
+use super::load_configs_from_file::ConfigsFromFile;
+
 pub struct Configs {
-    pub code_edit_model: String,
-    pub disable_prompt_validation: bool,
+    // CLI
     pub input_file: PathBuf,
     pub provider: String,
 
-    // Validation
+    // code editing
+    pub code_edit_model: String,
+
+    // validation
+    pub disable_prompt_validation: bool,
     pub ollama_host: String,
     pub ollama_port: u16,
     pub ollama_validation_model: String,
     pub validation_context_window: u16,
+    //configs: ConfigsFromFile,
 }
