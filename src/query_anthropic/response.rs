b118a0ebc4e0458d7477757ff6b07d883a101957
dsw7 <dds3443@hotmail.com>
2026-09-17 01:46:34 -0700

    Update response deserializer

--- src/query_anthropic/response.rs
+++ src/query_anthropic/response.rs
@@ -25,67 +25,27 @@ struct Error {
 
 #[derive(Deserialize, Debug)]
 struct Response {
-    // ResponseStatus string may or may not exist
-    status: Option<String>,
+    content: Vec<ContentBlock>,
+    usage: Usage,
 
-    // will always exist as object or null
-    incomplete_details: Option<IncompleteDetails>,
-
-    // will always exist
-    output: Vec<ResponseOutputMessage>,
-
-    // ResponseUsage object may or may not exist - populate with defaults
-    #[serde(default = "default_usage")]
-    usage: ResponseUsage,
+    // see https://platform.claude.com/docs/en/api/http/messages#stop_reason
+    stop_reason: String,
 }
 
 #[derive(Deserialize, Debug)]
-struct IncompleteDetails {
-    reason: String,
-}
+struct ContentBlock {
+    // see https://platform.claude.com/docs/en/api/http/messages#content_block
+    #[serde(rename = "type")]
+    content_type: String,
 
-fn unpack_incomplete_details(response: &Response) -> String {
-    match &response.incomplete_details {
-        Some(details) => format!("query incomplete: {}", details.reason),
-        None => String::from("query incomplete: no details provided"),
-    }
+    text: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
-struct ResponseOutputMessage {
-    status: String,
-    content: Vec<TextOrRefusal>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(untagged)]
-enum TextOrRefusal {
-    Text(ResponseOutputText),
-    Refusal(ResponseOutputRefusal),
-}
-
-#[derive(Deserialize, Debug)]
-struct ResponseOutputText {
-    text: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct ResponseOutputRefusal {
-    refusal: String,
-}
-
-fn unpack_output(response: &Response) -> anyhow::Result<StructuredOutput> {
-    if response.output.is_empty() {
-        anyhow::bail!("output array is empty");
-    }
-
-    for object in &response.output {
-        if object.status == "completed" {
-            return unpack_content(&object.content);
-        }
-    }
-
-    anyhow::bail!("query completed but no completed message found")
+struct Usage {
+    // see https://platform.claude.com/docs/en/api/http/messages#usage
+    input_tokens: u32,
+    output_tokens: u32,
 }
 
 #[derive(Deserialize, Debug)]
@@ -94,46 +54,40 @@ struct StructuredOutput {
     description_of_what_was_done: String,
 }
 
-fn unpack_content(content: &[TextOrRefusal]) -> anyhow::Result<StructuredOutput> {
-    if content.is_empty() {
-        anyhow::bail!("content array is empty");
-    }
-
-    let raw_text = match &content[0] {
-        TextOrRefusal::Text(text) => &text.text,
-        TextOrRefusal::Refusal(refusal) => anyhow::bail!("query refused: {}", refusal.refusal),
-    };
-
-    let structured_output = serde_json::from_str::<StructuredOutput>(raw_text)
+fn unpack_structured_output(text: &str) -> anyhow::Result<StructuredOutput> {
+    let structured_output = serde_json::from_str::<StructuredOutput>(&text)
         .context("failed to deserialize structured output")?;
 
     Ok(structured_output)
 }
 
-#[derive(Deserialize, Debug)]
-struct ResponseUsage {
-    input_tokens: u32,
-    output_tokens: u32,
-}
-
-fn default_usage() -> ResponseUsage {
-    ResponseUsage {
-        input_tokens: 0,
-        output_tokens: 0,
+fn unpack_text_block(response: &Response) -> anyhow::Result<StructuredOutput> {
+    if response.content.is_empty() {
+        anyhow::bail!("content array is empty");
     }
+
+    for content_block in &response.content {
+        // see https://platform.claude.com/docs/en/api/http/messages#text_block
+        if content_block.content_type == "text" {
+            if let Some(text) = &content_block.text {
+                return unpack_structured_output(text);
+            }
+        }
+    }
+
+    anyhow::bail!("no TextBlock object found in response")
 }
 
 fn unpack_response(response: &Response) -> anyhow::Result<AnthropicResults> {
-    let status = match &response.status {
-        Some(status) => status,
-        None => anyhow::bail!("no status could be found in response"),
-    };
+    if response.stop_reason != "end_turn" {
+        anyhow::bail!(format!(
+            "query stopped prematurely: {}",
+            response.stop_reason
+        ))
+    }
 
-    let structured_output = match status.as_str() {
-        "completed" => unpack_output(response)?,
-        "incomplete" => anyhow::bail!(unpack_incomplete_details(response)),
-        _ => anyhow::bail!(format!("query did not finish: {status}")),
-    };
+    let structured_output = unpack_text_block(response)
+        .context("something went wrong when unpacking structured output")?;
 
     let results = AnthropicResults {
         input_tokens: response.usage.input_tokens,
